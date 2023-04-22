7f968e0da183156160a55b0274be58032f9a8a44
TAY0123 <71745524+TAY0123@users.noreply.github.com>
2023-04-23 01:32:43 +0800

    shutdown flutter engine when in background mode

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -5,6 +5,8 @@ import SwiftUI
 
 @main
 struct FlutterApp: App {
+    @NSApplicationDelegateAdaptor(FlutterAppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             MyView()
@@ -19,12 +21,13 @@ struct MyView: NSViewControllerRepresentable {
     
     typealias NSViewControllerType = NSViewController
     
+    var close: (() -> Void)?
+    
     func makeNSViewController(context: Context) -> NSViewController {
         // Return MyViewController instance
         let mp = MediaPlayer.shared()
-        let engine = FlutterEngine(name: "a", project: nil)
-        print("engine: ", engine.run(withEntrypoint: nil))
-        let controller = FlutterViewController(engine: engine, nibName: nil, bundle: nil)
+        var c = FlutterDependencies()
+        let controller = FlutterViewController(engine: c.flutterEngine, nibName: nil, bundle: nil)
         RegisterGeneratedPlugins(registry: controller)
         let batteryChannel = FlutterMethodChannel(name: "samples.flutter.dev/mediaplayer",
                                                   binaryMessenger: controller.engine.binaryMessenger)
@@ -57,110 +60,24 @@ struct MyView: NSViewControllerRepresentable {
         
         return controller
     }
+    
+    static func dismantleNSViewController(_ nsViewController: Self.NSViewControllerType, coordinator: Self.Coordinator) {
+        let c = nsViewController as! FlutterViewController
+        c.engine.shutDownEngine()
+    }
 }
 
-class AppDelegate: FlutterAppDelegate {
-    private var eventSink: FlutterEventSink?
-    private final var mp = MediaPlayer.shared()
- 
-    // gui
-    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return false
+class AppDelegate: FlutterAppDelegate {}
+
+class FlutterDependencies: ObservableObject {
+    var flutterEngine = FlutterEngine(name: "flutterEngine", project: nil)
+    init() {
+        print("engine: ", flutterEngine.run(withEntrypoint: nil))
     }
- 
-    override func applicationWillBecomeActive(_ notification: Notification) {
-        print(notification.name)
-    }
- 
-    override func applicationDidFinishLaunching(_ notification: Notification) {
-        let controller: FlutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
-        let batteryChannel = FlutterMethodChannel(name: "samples.flutter.dev/mediaplayer",
-                                                  binaryMessenger: controller.engine.binaryMessenger)
- 
-        let playerStatusChannel = FlutterEventChannel(name: "samples.flutter.dev/mediaplayerStatus",
-                                                      binaryMessenger: controller.engine.binaryMessenger)
- 
-        playerStatusChannel.setStreamHandler(mp)
-        batteryChannel.setMethodCallHandler {
-            [weak mp] (call: FlutterMethodCall, result: @escaping FlutterResult) in
-            // This method is invoked on the UI thread.
-            switch call.method {
-            case "add":
-                mp?.replaceCurrent(url: call.arguments as! String, result: result, cacheMusic: false)
-            case "play":
-                mp?.play(result: result)
-            case "pause":
-                mp?.pause(result: result)
-            case "seek":
-                mp?.seek(to: call.arguments as! Int, result: result)
-            case "stop":
-                mp?.stop(result: result)
-            case "getPosition":
-                mp?.getPosition(result: result)
-            default:
-                result(FlutterMethodNotImplemented)
-                return
-            }
-        }
-    }
- 
-    override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
-        if !flag {
-            for window in NSApp.windows {
-                if !window.isVisible {
-                    window.setIsVisible(true)
-                }
-                reOpen(window: window)
-                window.makeKeyAndOrderFront(self)
-                NSApp.activate(ignoringOtherApps: true)
-            }
-        }
-        return true
-    }
- 
-    // end of gui
- 
-    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-        NotificationCenter.default.removeObserver(self)
-        eventSink = nil
-        return nil
-    }
- 
-    func reOpen(window: NSWindow) {
-        let engine = FlutterEngine(name: UUID().uuidString, project: nil)
-        print("engine: ", engine.run(withEntrypoint: nil))
-        let controller = FlutterViewController(engine: engine, nibName: nil, bundle: nil)
-        RegisterGeneratedPlugins(registry: controller)
-        window.contentViewController = controller
- 
-        let batteryChannel = FlutterMethodChannel(name: "samples.flutter.dev/mediaplayer",
-                                                  binaryMessenger: controller.engine.binaryMessenger)
- 
-        let playerStatusChannel = FlutterEventChannel(name: "samples.flutter.dev/mediaplayerStatus",
-                                                      binaryMessenger: controller.engine.binaryMessenger)
- 
-        playerStatusChannel.setStreamHandler(mp)
-        batteryChannel.setMethodCallHandler {
-            [weak mp] (call: FlutterMethodCall, result: @escaping FlutterResult) in
-            // This method is invoked on the UI thread.
-            switch call.method {
-            case "add":
-                mp?.replaceCurrent(url: call.arguments as! String, result: result, cacheMusic: false)
-            case "play":
-                mp?.play(result: result)
-            case "pause":
-                mp?.pause(result: result)
-            case "seek":
-                mp?.seek(to: call.arguments as! Int, result: result)
-            case "stop":
-                mp?.stop(result: result)
-            case "getPosition":
-                mp?.getPosition(result: result)
-            default:
-                result(FlutterMethodNotImplemented)
-                return
-            }
-        }
+
+    func re() {
+        flutterEngine = FlutterEngine(name: "flutterEngine", project: nil)
+        print("engine: ", flutterEngine.run(withEntrypoint: nil))
     }
 }
- 
+
--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,29 +1,31 @@
 import Cocoa
 import FlutterMacOS
 
-class MainFlutterWindow: NSWindow {
-    private var controller: FlutterViewController?
-  
-    var mp: MediaPlayer = .shared()
-    
-    override func restoreUserActivityState(_ userActivity: NSUserActivity) {}
-    
-    override func awakeFromNib() {
-        let engine = FlutterEngine(name: "a", project: nil)
-        print("engine: ", engine.run(withEntrypoint: nil))
-        controller = FlutterViewController(engine: engine, nibName: nil, bundle: nil)
-
-        let windowFrame = frame
-        contentViewController = controller
-        setFrame(windowFrame, display: true)
-        RegisterGeneratedPlugins(registry: controller!)
-
-        super.awakeFromNib()
-    }
-    
-    override func close() {
-        print("engine closed")
-        controller?.engine.shutDownEngine()
-        super.close()
-    }
-}
+/*
+ class MainFlutterWindow: NSWindow {
+ private var controller: FlutterViewController?
+ 
+ var mp: MediaPlayer = .shared()
+ 
+ override func restoreUserActivityState(_ userActivity: NSUserActivity) {}
+ 
+ override func awakeFromNib() {
+ let engine = FlutterEngine(name: "a", project: nil)
+ print("engine: ", engine.run(withEntrypoint: nil))
+ controller = FlutterViewController(engine: engine, nibName: nil, bundle: nil)
+ 
+ let windowFrame = frame
+ contentViewController = controller
+ setFrame(windowFrame, display: true)
+ RegisterGeneratedPlugins(registry: controller!)
+ 
+ super.awakeFromNib()
+ }
+ 
+ override func close() {
+ print("engine closed")
+ controller?.engine.shutDownEngine()
+ super.close()
+ }
+ }
+ */
